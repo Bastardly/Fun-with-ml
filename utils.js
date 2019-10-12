a7ab2dfaec7ca06e514ab471f9759e6a54df60b1
Flemming Hansen <flemshans@gmail.com>
2019-10-12 21:50:51 +0200

    Let's make our problem a bit harder

--- utils.js
+++ utils.js
@@ -6,9 +6,11 @@ function generateSet(count, collection) {
   let x = 0;
   for (x = 0; x < count; x++) {
     const x1 = getRandomNumber();
+    const x2 = getRandomNumber();
+    const x3 = getRandomNumber();
     collection.push({
-      coordSet: [x1, getRandomNumber(), getRandomNumber()], // Now we are working in three dimensions - but it could be n dimensions
-      isAccepted: x1 > 0 // So this is really basic stuff our "neural network" should find out;
+      coordSet: [x1, x2, x3], // Now we are working in three dimensions - but it could be n dimensions
+      isAccepted: x1 > 0 && x2 < 0 && x3 < 5 // So this is the "stuff" our "neural network" should find out;
     });
   }
 }
